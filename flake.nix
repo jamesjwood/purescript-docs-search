e32fa7268953e0f1e210c57de0241282137dcaed
Build Service <build@cambridge-vision.tech>
2025-11-03 12:44:27 +0000

    Update to use ARM64-enabled purs-nix fork
    Point to jamesjwood/purs-nix/arm64-internal which includes
    ARM64 support for the complete PureScript ecosystem.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
         };
 
       ps-tools.follows = "purs-nix/ps-tools";
-      purs-nix.url = "github:purs-nix/purs-nix/ps-0.15";
+      purs-nix.url = "github:jamesjwood/purs-nix/arm64-internal";
       utils.url = "github:ursi/flake-utils/8";
     };
 
